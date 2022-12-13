9cfd2919bcb7ae471270991e480dbc9e8d66cb61
Rokas <rookas.rudzenskas@gmail.com>
2022-12-13 16:20:44 +0200

    Add fonts and global styles
    - Add fonts to public folder
    - Add font-face to global styles
    - Add font-family to tailwind config
    - Add font-family to global styles
    - Add height to tailwind config
    - Add height to global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <div>
+      <div className="py-24 px-12 lg:px-48 font-latoBold">
         <Navbar />
         <Component {...pageProps} />
       </div>
--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { motion as m } from 'framer-motion';
 
 const Contact = ({}) => {
     return (
-        <div>
-            
-        </div>
+        <main>
+            <div className="my-96 p-1">
+                <h1 className="text-6xl text-center lg:text-right lg:text-9xl">Let's Talk</h1>
+            </div>
+            <div className="flex gap-40">
+                <div className="lg:text-2xl text-base">
+                    <h4>Find me:</h4>
+                </div>
+                <div className="lg:text-6xl text-2xl underline">
+                    <ul>
+                        <li className="pb-2">Twitter</li>
+                        <li className="pb-2">Instagram</li>
+                        <li className="pb-2">LinkedIn</li>
+                        <li className="pb-2">Dribble</li>
+                    </ul>
+                </div>
+            </div>
+        </main>
     );
 };
 
